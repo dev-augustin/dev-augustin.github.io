6f72c2cbaf278ecf4ac172d87eaa0ee8430b9443
dev-augustin <dev.augustin00@gmail.com>
2020-05-29 18:48:46 -0500

    search with rapid api

--- GiftStore/frontend/src/components/Search.js
+++ GiftStore/frontend/src/components/Search.js
@@ -5,21 +5,21 @@ const API_KEY=process.env.REACT_APP_API_KEY;
 console.log(API_KEY);
 
 
-const params={
-    api_key: API_KEY,
-    type: "category",
-    language: "en_US",
-    customer_location: "us",
-    url: "https://www.amazon.com/s?k=birthday+gifts&rh=n%3A16310101%2Cn%3A2255576011&dc&qid=1590771862&rnid=2941120011&ref=sr_nr_n_2"
-}
+// const params={
+//     api_key: API_KEY,
+//     type: "category",
+//     language: "en_US",
+//     customer_location: "us",
+//     url: "https://www.amazon.com/s?k=birthday+gifts&rh=n%3A16310101%2Cn%3A2255576011&dc&qid=1590771862&rnid=2941120011&ref=sr_nr_n_2"
+// }
 
 
 export default class Search extends Component {
     constructor(props){
         super(props);
         this.state = {
-            arrays: [],
-            results: ""
+            order: [],
+            results: []
         }
     }
 
@@ -29,22 +29,79 @@ export default class Search extends Component {
     search = async () =>{
 
         try{
+const response= await axios.get('https://asos2.p.rapidapi.com/products/v2/list', { 
+    "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"asos2.p.rapidapi.com",
+        "x-rapidapi-key":API_KEY,
+        "useQueryString":true,
+    },"params":{
+        "country":"US",
+        "currency":"USD",
+        "sort":"freshness",
+        "lang":"en-US",
+        "sizeSchema":"US",
+        "offset":"0",
+        "categoryId":"4209",
+        "limit":"48",
+        "store":"US"
+        }
+    }    
+)
 
-            const response= await axios.get('https://api.rainforestapi.com/request', { params })
             console.log(response)
-            console.log("data: ",response.data);
-            console.log("category: ",response.data.category_results[0]);
-            console.log("asin: ",response.data.category_results[0].asin);
-            console.log("image ",response.data.category_results[0].image);
-            console.log("title ",response.data.category_results[0].title);
-            console.log("name ",response.data.category_results[0].categories[0].name);
-            console.log("name ",response.data.category_results[0].prices[0].raw);
-            console.log("categoryAll: ",response.data.category_results);
-        }
+    //         axios({
+    //             "method":"GET",
+    //             "url":"https://asos2.p.rapidapi.com/products/v2/list",
+    //             "headers":{
+    //             "content-type":"application/octet-stream",
+    //             "x-rapidapi-host":"asos2.p.rapidapi.com",
+    //             "useQueryString":true
+    // },"params":{
+    // "country":"US",
+    // "currency":"USD",
+    // "sort":"freshness",
+    // "lang":"en-US",
+    // "sizeSchema":"US",
+    // "offset":"0",
+    // "categoryId":"4209",
+    // "limit":"48",
+    // "store":"US"
+    // }
+    //         )
+                  
+//     "method":"GET",
+//     "url":"https://asos2.p.rapidapi.com/categories/list",
+//     "headers":{
+       
+//             "content-type":"application/octet-stream",
+//             "x-rapidapi-host":"asos2.p.rapidapi.com",
+//     "x-rapidapi-key":API_KEY,
+//     "useQueryString":true
+// },"params":{
+//     "country":"US",
+//     "lang":"en-US"
+//     }
+    }
+    catch(e){
+
+    }
+
+            // const response= await axios.get('https://api.rainforestapi.com/request', { params })
+            // console.log(response)
+            // console.log("data: ",response.data);
+            // console.log("category: ",response.data.category_results[0]);
+            // console.log("asin: ",response.data.category_results[0].asin);
+            // console.log("image ",response.data.category_results[0].image);
+            // console.log("title ",response.data.category_results[0].title);
+            // console.log("name ",response.data.category_results[0].categories[0].name);
+            // console.log("name ",response.data.category_results[0].prices[0].raw);
+            // console.log("categoryAll: ",response.data.category_results);
+        // }
             
-        catch(e){
-            console.log(e)
-        }
+        // catch(e){
+        //     console.log(e)
+        // }
  
   
    
