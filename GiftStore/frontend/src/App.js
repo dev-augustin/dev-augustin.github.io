dc0e823782b7455185dabe24f9e54025dba3cc70
dev-augustin <dev.augustin00@gmail.com>
2020-06-01 00:33:36 -0500

    updated route

--- GiftStore/frontend/src/App.js
+++ GiftStore/frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.scss'
-import Search from './components/Search'
+// import Search from './components/Search'
 import MyCart from './components/MyCart'
 import MyProfile from './components/MyProfile'
 import Handmade from './components/Handmade'
@@ -12,6 +12,9 @@ import Navigation from './components/Navigation'
 import Home from  './components/Home'
 import Review from './components/Review'
 import Contact from  './components/Contact'
+import View from  './components/View'
+import PartySupplies from  './components/PartySupplies'
+import EditOrder from './components/EditOrder'
 
 function App() {
   return (
@@ -22,11 +25,16 @@ function App() {
             <Route exact path = '/' component={Home}></Route>
             <Route exact path = '/myCart' component={MyCart}></Route>
             <Route exact path = '/myProfile' component={MyProfile}></Route>
-            <Route exact path = '/giftBasket' component={GitftBasket}></Route>
+            {/* <Route exact path = '/giftBasket/3/3' component={GitftBasket}></Route> */}
             <Route exact path = '/flowers/3.2' component={Flowers}></Route>
             <Route exact path = '/handmade/3.1' component={Handmade}></Route>
             <Route exact path = '/review' component={Review}></Route>
             <Route exact path = '/contact' component={Contact}></Route>
+            <Route exact path = '/view' component={View}></Route>
+            <Route exact path = '/editorder/:id' component={EditOrder}></Route>
+            
+            
+            <Route exact path = '/partySupplies/3.3' component={PartySupplies}></Route>
          </Switch>
 
        </div>
